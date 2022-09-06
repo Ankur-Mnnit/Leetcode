8ad34ec52eb313d18c1448b8f65f588def50da95
Ankur-Mnnit <Ankur-Mnnit>
2022-09-07 00:27:09 +0530

    commit

---
+++ DSA 150 important questions/pairs.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+void pairs(vector<int> v,int target){
+    unordered_map<int,int> m;
+    for(int i=0;i<v.size();i++){
+        if(m.find(v[i]-target)!=m.end()){
+            cout<<i<<m[v[i]-target];
+        }
+        m[v[i]]=i;
+    }
+    
+}
+
+int main(){
+    vector<int> v={1,2,3,4,5,6,7,8};
+    pairs(v,5);
+}
