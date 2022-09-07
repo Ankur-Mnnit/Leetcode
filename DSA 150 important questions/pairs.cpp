261a1a2dbe6942131e1521f1433a0991a56b84eb
unknown <aknarayan02@gmail.com>
2022-09-07 06:28:27 +0530

    commit

--- DSA 150 important questions/pairs.cpp
+++ DSA 150 important questions/pairs.cpp
@@ -7,12 +7,12 @@ void pairs(vector<int> v,int target){
         if(m.find(v[i]-target)!=m.end()){
             cout<<i<<m[v[i]-target];
         }
-        m[v[i]]=i;
+        m[v[i]=i;
     }
     
 }
 
 int main(){
-    vector<int> v={1,2,3,4,5,6,7,8};
+    vector<int> v={1,2,3,4,5,6,7,8,9};
     pairs(v,5);
 }
